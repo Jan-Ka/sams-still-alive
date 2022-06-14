3166fda3ff9f23de0c000104d745deabd8d888f6
Jan Kaltenecker <Jan-Ka@users.noreply.github.com>
2022-06-14 12:06:25 +0000

    rework for simpler usecase

--- src/index.js
+++ src/index.js
@@ -1,219 +1,65 @@
-import { AutoQueue } from './AutoQueue.mjs';
-import { Writer } from './Writer.mjs';
+const speech = window.speechSynthesis;
 
-run(document.getElementById("content"), document.getElementById("control"));
+/** @type {HTMLButtonElement} */
+const controlButton = document.getElementById("control");
+/** @type {HTMLDivElement} */
+const settings = document.getElementById("settings");
+/** @type {HTMLSelectElement} */
+const settingsSelect = document.getElementById("voice");
 
-/**
- * 
- * @param {HTMLDivElement} parent 
- * @param {HTMLButtonElement} control 
- */
-async function run(parent, control) {
-    const contentContainer = parent;
-    const controlButton = control;
+controlButton.focus();
 
-    const globQueue = new AutoQueue();
-    const globWriter = new Writer(contentContainer);
+let voiceOptions = [];
 
-    globQueue.enqueue(playIntro(globWriter, controlButton));
-}
+speech.onvoiceschanged = () => {
+    const voices = speech.getVoices();
 
-/**
- * 
- * @param {Writer} writer 
- * @param {HTMLButtonElement} button
- */
-function playIntro(writer, button) {
-    const sceneQueue = new AutoQueue();
-
-    button.addEventListener("click", () => {
-        console.log("click");
-        sceneQueue.stop();
-    });
-
-    const actions = [
-        () => writer.clear(),
-        // () => { writer.delay = 25; },
-        "Press the ◉ button (or your enter key) to begin setup.",
-        () => { button.disabled = false; button.focus(); },
-        "",
-        // () => { writer.delay = 100; },
-        "This is required because modern Browsers save you from having random crap played",
-        "",
-        "Aren't they nice?"
-    ];
-
-    handleLine(writer, actions, sceneQueue);
-
-    return () => { sceneQueue.start() };
-}
-
-/**
- * 
- * @param {Writer} writer
- * @param {(string|Function)[]} lines 
- * @param {AutoQueue} queue 
- */
-function handleLine(writer, lines, queue) {
-    for (const line of lines) {
-        const lineType = typeof line;
-
-        switch (lineType) {
-            case "string":
-                if (line.trim().length === 0) {
-                    queue.enqueue(() => writer.newLine(), false);
-                    break;
-                }
-
-                const words = line.split(" ");
-                if (words.length > 5) {
-                    const lastWord = words.splice(-1)[0];
-
-                    for (const word of words) {
-                        queue.enqueue(() => writer.write(`${word} `, false), false);
-                    }
-
-                    queue.enqueue(() => writer.write(lastWord), false)
-
-                    break;
-                }
-                queue.enqueue(() => writer.write(line), false);
-                break;
-            case "function":
-                queue.enqueue(line, false);
-                break;
-            default:
-                console.debug(`Unhandled line type ${lineType}`, line);
-                break;
-        }
+    for (const voice of voices) {
+        voiceOptions.push([voice.name, voice.lang, voice.default]);
     }
 }
 
-// async function run(parent) {
-//     const contentContainer = parent;
-//     const control = document.getElementById("control")
+controlButton.onclick = (e) => {
+    settings.classList.remove("hidden");
 
-//     const globQueue = new Queue();
+    if (voiceOptions.length === 0) {
+        settingsSelect.innerHTML = `<option>No Voices available. Press the ◉ Button Again.</option>`;
+        settingsSelect.disabled = true;
+        return;
+    }
 
-//     // not really happy doing it this way
-//     // but it is convenient and allows to
-//     // overwrite the target if required
-//     newLine = newLine.bind({ target: contentContainer });
-//     getCurrentCursorSpace = getCurrentCursorSpace.bind({ target: contentContainer });
+    voiceOptions.sort((a, b) => {
+        const compareLang = a[1].localeCompare(b[1]);
+        if (compareLang == 0) {
+            return a[0].localeCompare(b[0]);
+        }
 
-//     control.addEventListener("click", async (e) => {
-//         // const target = e.target;
+        return compareLang;
+    });
 
-//         globQueue.interrupt();
+    for (const voiceOption of voiceOptions) {
+        const option = document.createElement("option");
+        option.textContent = `${voiceOption[0]} (${voiceOption[1]})`;
+        option.dataset.name = voiceOption[0];
+        option.dataset.lang = voiceOption[1];
 
-//         await playSetup(contentContainer, control, globQueue);
-//         // const isPlaying = target.dataset.playing === "true";
+        if (voiceOption[2] === true) {
+            option.selected = true;
+        }
 
-//         // if (isPlaying) {
-//         //     target.innerHTML = "▶";
-//         //     target.dataset.playing = false;
-//         // } else {
-//         //     target.innerHTML = "■";
-//         //     target.dataset.playing = true;
-//         // }
-//     });
+        settingsSelect.appendChild(option);
+    }
 
-//     await playIntro(contentContainer, control, globQueue);
-// }
+    settingsSelect.disabled = false;
 
-// async function playIntro(parent, control, queue) {
-//     queue.enqueue(() => { clear(parent) });
+    setControlIcon();
+}
 
-//     const introLines = [
-//         [
-//             // "Press the ◉ button (or your enter key) to begin setup.", async () => {
-//             //     await wait(100);
-//             //     control.disabled = false;
-//             //     control.focus();
-//             // }],
-//             "Press t", async () => {
-//                 await wait(100);
-//                 control.disabled = false;
-//                 control.focus();
-//             }],
-//         "This is required because modern Browsers save you from having random crap played",
-//         "",
-//         "Aren't they nice?"
-//     ];
+function setControlIcon() {
+    if (controlButton.dataset.playing === "true") {
+        controlButton.textContent = "■";
+        return;
+    }
 
-//     for (const line of introLines) {
-//         handle(line, queue);
-//     }
-
-//     console.debug("poke intro");
-//     await queue.poke();
-// }
-
-// async function playSetup(parent, control, queue) {
-//     queue.enqueue(() => { clear(parent) });
-
-//     control.disabled = true;
-
-//     const setupLines = [
-//         "Alright, let's setup our wonderful singing voice first.",
-//         ""
-//     ];
-
-//     for (const line of setupLines) {
-//         handle(line, queue);
-//     }
-
-//     console.debug("poke setup");
-//     await queue.poke();
-// }
-
-// /**
-//  * 
-//  * @param {String | Array} line 
-//  * @param {Queue} queue 
-//  * @returns 
-//  */
-// async function handle(line, queue) {
-
-//     // allows to interrupt earlier
-//     const perWordEnqueue = (line) => {
-//         const words = line.split(" ");
-//         const lastWord = words.splice(-1, 1);
-
-//         for (const word of words) {
-//             queue.enqueue(() => { return write(`${word} `, false) });
-//         }
-
-//         queue.enqueue(() => { return write(lastWord[0]) });
-//     };
-
-//     // shortcut for simple lines
-//     if (typeof line === "string") {
-//         // queue.enqueue(() => { return write(line) });
-//         perWordEnqueue(line);
-//         return;
-//     }
-
-//     if (!Array.isArray(line)) {
-//         console.log("And what am I supposed to do with that?", line);
-//         return;
-//     }
-
-//     for (const item of line) {
-//         const itemType = typeof item;
-//         switch (itemType) {
-//             case "string":
-//                 // queue.enqueue(() => { return write(item) });
-//                 perWordEnqueue(item);
-//                 break;
-//             case "function":
-//                 queue.enqueue(() => { return item() });
-//                 break;
-//             default:
-//                 console.log(`Unhandled type ${itemType}`, item);
-//         }
-//     }
-// }
-
-
-
+    controlButton.textContent = "▶";
+}
